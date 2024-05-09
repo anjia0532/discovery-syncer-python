bc51f9e6415de54888dcd0ab0230ffdf46465b79
AnJia <anjia0532@gmail.com>
2024-05-09 10:23:03 +0800

    apisix 生成 apisix db-less 文件时，去除 update_time 参数

--- app/service/gateway/apisix.py
+++ app/service/gateway/apisix.py
@@ -159,6 +159,7 @@ class Apisix(Gateway):
                 item_val = vv.get("value", vv)
                 if item_val.get("status", 1) == 0:
                     continue
+                item_val.pop("update_time", None)
                 val[item.get("field")].append(item_val)
 
         yaml.SafeDumper.org_represent_str = yaml.SafeDumper.represent_str
